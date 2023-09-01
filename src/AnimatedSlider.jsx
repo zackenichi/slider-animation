583da9fff8f37168b19e0de0be0fb7164f3f454a
zackenichi <thekenichi26@gmail.com>
2023-09-01 21:05:11 +0800

    slider animation on load

---
+++ src/AnimatedSlider.jsx
@@ -0,0 +1,57 @@
+import { Slider } from '@mui/material';
+import React, { useEffect } from 'react';
+
+const AnimatedSlider = ({ minLabel, maxLabel }) => {
+  const [value, setValue] = React.useState(0);
+  const targetValue = 50;
+
+  const marks = [
+    {
+      value: 0,
+      label: minLabel,
+    },
+    {
+      value: 100,
+      label: maxLabel,
+    },
+  ];
+
+  const handleChange = (_event, newValue) => {
+    setValue(newValue);
+  };
+
+  //   React.useEffect(() => {
+  //     if (value < targetValue) {
+  //       setValue(value + 1);
+  //     }
+  //   }, [value]);
+
+  useEffect(() => {
+    const animationTimeout = setTimeout(() => {
+      setValue(targetValue);
+    }, 100); // Adjust the delay as needed
+
+    return () => {
+      clearTimeout(animationTimeout);
+    };
+  }, []); // Empty dependency array to run this effect only once
+
+  return (
+    <Slider
+      aria-label="Volume"
+      value={value}
+      onChange={handleChange}
+      marks={marks}
+      min={0}
+      max={100}
+      sx={{
+        '& .MuiSlider-markLabel': {
+          fontSize: '10px',
+          color: 'gray',
+        },
+      }}
+    />
+  );
+};
+
+export default AnimatedSlider;
